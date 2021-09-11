24108ba8f3107aa1f76c5b55a06d320b9940f422
samuVillegas <villegassamuel25@gmail.com>
2021-09-11 18:08:48 -0500

    Configuración carpetas backend e inicio de proyecto frontend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
-const port = 3000;
+
+
+/* Settings */
+app.set('port', process.env.PORT || 3000)
+
+/* Middlewares */
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(app.get('port'), () => {
+  console.log(`Example app listening at http://localhost:${app.get('port')}`);
 });
