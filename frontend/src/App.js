8ff4da410b432cdbbf1294d387dfaa12b81059f7
samuVillegas <villegassamuel25@gmail.com>
2021-11-01 08:55:09 -0500

    Se configura la ruta

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import Index from "./containers/Dashboard/index"
-import Posts from "./containers/Dashboard/posts";
+import Posts from "./containers/Dashboard/posts"
 import Signin from "./containers/Signin/index"
 import Signup from "./containers/Signup/index"
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-​
-​
-​
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Redirect exact from={`/`} to={`/signin`}/>
         <Route exact path={'/signin'} component={Signin} />
         <Route exact path={'/dashboard/index'} component={Index} />
         <Route exact path={'/dashboard/posts'} component={Posts} />
@@ -18,5 +18,6 @@ function App() {
     </BrowserRouter>
   );
 }
-​
+
+
 export default App;
